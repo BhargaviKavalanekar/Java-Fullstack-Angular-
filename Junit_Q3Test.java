1e122b0970f817807e491eab3a0688103344f1bb
BhargaviKavalanekar <86184476+BhargaviKavalanekar@users.noreply.github.com>
2021-08-16 01:13:21 +0530

    Add files via upload

---
+++ Junit_Q3Test.java
@@ -0,0 +1,16 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class Junit_Q3Test {
+	
+		@Test
+		void test() {
+			Junit_Q3 ob= new Junit_Q3(17000);
+			assertThrows(InsufficientFundsException.class, () -> ob.withdraw(20000));
+		}
+
+	
+}
