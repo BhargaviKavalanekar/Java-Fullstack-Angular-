617fef58a3706ca9b4ce5054e601cd06edd78dd8
BhargaviKavalanekar <86184476+BhargaviKavalanekar@users.noreply.github.com>
2021-08-30 20:04:07 +0530

    REST Assignment

---
+++ Spring/Spring/REST/SpringREST-2/src/main/java/com/example/bk/Que9/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.bk.Que9;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.bk.Que5.Employee;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+}
